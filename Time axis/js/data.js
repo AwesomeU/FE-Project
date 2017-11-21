a70e93dfec6715cfd4c4d31109884246b2955739
async <async>
2017-11-21 14:59:14 +0800

    time axis

---
+++ Time axis/js/data.js
@@ -0,0 +1,93 @@
+var data = [];
+
+data.push({
+    'date'  : '2014-7-31',
+    'intro' : 'Bognor Regis 英国的天蓝蓝的 和中国的雾霾简直没得比 University非常漂亮 都是欧洲风格的建筑 人不多很清静 图书馆也很赞 环境很好',
+    'media' : '<img src="image/img1.jpg" class="my_img" width="380" >',
+    'like' : 302,
+    'comment' : 883
+})
+
+
+data.push({
+    'date'  : '2014-8-1',
+    'intro' : '太靠近北极圈的关系这里晚上十点还和咱那下午四点一样亮 所以晚上十点小伙伴们在英吉利海峡扔石子',
+    'media' : '<img src="image/img4.jpg" class="my_img" width="380" >',
+    'like' : 35038,
+    'comment' : 817
+})
+
+data.push({
+    'date'  : '2014-8-2',
+    'intro' : '早晨吃的烤面包 加上各种果酱 ~ 比昨天的麦片泡牛奶好太多了 - - ~ 上午BOBO同学还迷路了，大家伙都在奇切斯特 那货竟然自己坐公交跑到伦敦去了 - - ',
+    'media' : '<img src="image/img5.jpg" class="my_img" width="380" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-8-3',
+    'intro' : 'Postmouth 炒鸡大的港口，盆友们一起shopping，body shop的小票围起来可绕地球一圈',
+    'media' : '<img src="image/img6.jpg" class="my_img" width="380" >',
+    'like' : 2538,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-8-4',
+    'intro' : '最爱的Teacher 年级很大但对我们满满的爱',
+    'media' : '<img src="image/img6.JPG" class="my_img" width="380" >',
+    'like' : 25038,
+    'comment' : 417
+})
+data.push({
+    'date'  : '2016-10-2',
+    'intro' : '最爱的Teacher 年级很大但对我们满满的爱',
+    'media' : '<img src="image/image4.JPG" class="my_img" width="380" >',
+    'like' : 25038,
+    'comment' : 417
+})
+data.push({
+    'date'  : '2014-9-1',
+    'intro' : '深夜的一抹身影',
+    'media' : '<img src="image/img7.jpg" class="my_img" width="380" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-9-2',
+    'intro' : 'Manchester 曼联球场作为伪球迷拿着V.PERSE的20号球衣和一堆球粉们拍照',
+    'media' : '<img src="image/img9.jpg" class="my_img" width="380" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-9-03',
+    'intro' : '终于实现脚踏东西半球的梦想',
+    'media' : '<img src="image/img8.jpg" class="my_img" width="380" >',
+    'like' : 238,
+    'comment' : 417
+})
+data.push({
+    'date'  : '2014-9-4',
+    'intro' : 'hahaha',
+    'media' : '<img src="image/img10.jpg"class="my_img" width="380" >',
+    'like' : 32102,
+    'comment' : 883
+})
+data.push({
+    'date'  : '2014-9-6',
+    'intro' : 'ha',
+    'media' : '<img src="image/image4.JPG"class="my_img" width="380" >',
+    'like' : 32102,
+    'comment' : 883
+})
+data.push({
+    'date'  : '2014-9-5',
+    'intro' : '终于来了心心念念的地方——日不落帝国的大本钟',
+    'media' : '<img src="image/img10.jpg"class="my_img" width="380" >',
+    'like' : 32102,
+    'comment' : 883
+})
